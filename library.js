329bb785fffff70650795d4e7cd6b181a3516e39
Eric Pratt <zealot2323@gmail.com>
2025-02-17 16:33:29 +0100

    Added class

--- library.js
+++ library.js
@@ -1,14 +1,18 @@
 const myLibrary = [];
 
-function Book (title, author, pages, read) {
+class Book {
+    constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
+}
+
+    info() {
         return `${this.title} by ${this.author} has ${this.pages} and has been ${this.read}.`;
     }
 };
+ 
 
 function addBookToLibrary(title, author, pages, read) {
     let newBook = new Book (title, author, pages, read);
